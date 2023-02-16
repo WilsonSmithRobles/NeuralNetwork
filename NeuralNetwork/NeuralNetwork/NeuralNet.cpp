c9aea9f18c9ef34d88ea0639d363721f1181c63e
WilsonSmithRobles <robleschicawilsonsmith@gmail.com>
2023-02-16 01:58:33 +0100

    Added Basic Definitions and transfer functions

--- NeuralNetwork/NeuralNetwork/NeuralNet.cpp
+++ NeuralNetwork/NeuralNetwork/NeuralNet.cpp
@@ -1,1 +1,35 @@
 #include "NeuralNet.h"
+
+
+//###########################################################Neuron Transfer Functions#################################################
+double NeuronTransferFunctions::hyperbolicTangent(double x)
+{
+	return tanh(x);
+}
+double NeuronTransferFunctions::hyperbolicTangentDerivative(double x)
+{
+	double result = 1 - tanh(x) * tanh(x);
+	return result;
+}
+double NeuronTransferFunctions::sigmoid(double x)
+{
+	double result = 1 / (1 + exp(-x));
+	return result;
+}
+double NeuronTransferFunctions::sigmoid_derivative(double x)
+{
+	double result = sigmoid(x) * (1 - sigmoid(x));
+	return result;
+}
+double NeuronTransferFunctions::ReLU_Function(double x)
+{
+	if (x > 0)
+		return x;
+	return 0;
+}
+double NeuronTransferFunctions::ReLU_Derivative(double x)
+{
+	if (x > 0)
+		return 1;
+	return 0;
+}
--- NeuralNetwork/NeuralNetwork/NeuralNet.h
+++ NeuralNetwork/NeuralNetwork/NeuralNet.h
@@ -1,5 +1,51 @@
 #pragma once
+
+#include <vector>
+#include <math.h>
+
+struct NeuronTransferFunctions
+{
+	double hyperbolicTangent(double x);
+	double hyperbolicTangentDerivative(double x);
+	double sigmoid(double x);
+	double sigmoid_derivative(double x);
+	double ReLU_Function(double x);
+	double ReLU_Derivative(double x);
+
+};
+
+
+struct Connection
+{
+	double weight, deltaWeight;
+};
+typedef std::vector<Neuron> Layer;
+
+class Neuron {
+public:
+	Neuron(size_t transferFunctionUsed);
+	~Neuron();
+	double get_output();
+
+
+private:
+	double transferFunction(double Input);
+	double transferFunctionDerivative(double x);
+	double my_output;
+	std::vector<Connection> outConnections;
+
+};
+
 class NeuralNet
 {
+public:
+	NeuralNet();
+	~NeuralNet();
+	void feedForward(std::vector<double> Inputs);
+	void backPropagation(std::vector<double> Targets);
+	int getMaximizedOutput();
+
+private:
+
 };
 
