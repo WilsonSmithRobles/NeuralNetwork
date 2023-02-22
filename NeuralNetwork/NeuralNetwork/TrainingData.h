82f841dd963c18534fa56fcda826fd6d77279318
WilsonSmithRobles <robleschicawilsonsmith@gmail.com>
2023-02-22 19:02:14 +0100

    Defined header for TrainingData class.

--- NeuralNetwork/NeuralNetwork/TrainingData.h
+++ NeuralNetwork/NeuralNetwork/TrainingData.h
@@ -1,5 +1,32 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+
+struct LabeledInputs
+{
+	std::vector<double> inputs;
+	std::vector<double> targets;
+};
+
 class TrainingData
 {
+	TrainingData(std::string csv_samplesFile, std::string csv_topology_and_functions);
+	~TrainingData(void);
+
+public:
+	std::vector<unsigned> topology;
+	std::vector<unsigned> transferFunctions;
+
+	LabeledInputs getShuffledTrainingVectors(void);
+	LabeledInputs getShuffledAutoevalVectors(void);
+	LabeledInputs getShuffledValidationVectors(void);
+
+private:
+	LabeledInputs allSamples;
+	LabeledInputs Training;
+	LabeledInputs Autoevaluation;
+	LabeledInputs Validation;
+	void shuffleAndSeparateSamplesInGroups(void);
 };
 
